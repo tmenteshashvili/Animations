7dd487e3864daa4895b48c78f5edae430dd30136
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2024-01-17 19:53:46 +0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Tako Menteshashvili on 17.01.24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
